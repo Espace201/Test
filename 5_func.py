30bba6f78040b36d1a35a508ecd58b8e5eb10956
Sergey <seryoga@gmail.com>
2022-05-20 03:55:48 +0300

    Дописать в конец файла две строки

---
+++ 5_func.py
@@ -0,0 +1,7 @@
+a, b, c, d = 'А я сейчас вам покажу,\n','откуда на Беларусь\n', 'готовилось нападение.\n', 'И если бы за шесть часов до операции...'
+file = open('C:/Users/Sergey/PycharmProjects/Test2/les6_2.txt', 'a', encoding='utf-8')
+file.writelines([c, c])
+file.close()
+
+
+
