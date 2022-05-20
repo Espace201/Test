0636dc9a7846dd9e4bbe2e030953eda51ce5f62a
Sergey <seryoga@gmail.com>
2022-05-20 03:49:46 +0300

    Введите число. Больше, меньше, равно нулю, с плавающей запятой.

---
+++ float_number.py
@@ -0,0 +1,15 @@
+a = input('Введите число: ')
+b = a.count('.')
+if a.isdigit() or b == 1 or a[0] == '-' and a[1:].isdigit():
+    a = float(a)
+
+    if a > 0:
+        print('Число положительное')
+    elif a < 0:
+        print('Число отрицательное')
+    elif a == 0:
+        print('Ноль')
+
+else:
+    print('Введите число')
+
